598b3fe9a70d9aade936a4696aadd5b87c64d330
raoxj <1471991623@qq.com>
2021-01-25 15:26:01 +0800

    增加了Person类，并在其中添加age属性

---
+++ TestModule01/src/com/rxj/Person.java
@@ -0,0 +1,11 @@
+package com.rxj;
+
+/**
+ * @Auther:raoxiaojun
+ * @Date:2021/1/25 - 01 - 25 - 15:22
+ * @Description:com.rxj
+ * @version:1.0
+ */
+public class Person {
+    private int age;//
+}
